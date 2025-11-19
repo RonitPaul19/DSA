160c654387b267a029c3382213298d1725ad90cf
Ronit Paul <ronitpaulindia@gmail.com>
2025-11-20 00:44:01 +0530

    debug

--- 01_ARRAYS/02_Questions/07_union.cpp
+++ 01_ARRAYS/02_Questions/07_union.cpp
@@ -16,51 +16,57 @@ vector<int> UNION_Brute(vector<int> a, vector<int> b)
 }
 
 // OPTIMAL APPROACH (two-pointer method)
-vector<int> FindUnion(int arr1[], int arr2[], int n, int m)
-{
+vector<int> FindUnion(vector<int> a, vector<int> b) {
+  int n1 = a.size();
+  int n2 = b.size();
   int i = 0, j = 0;
-  vector<int> Union;
+  vector<int> unionArr;
 
-  while (i < n && j < m)
-  {
-    if (arr1[i] <= arr2[j])
-    {
-      if (Union.empty() || Union.back() != arr1[i])
-        Union.push_back(arr1[i]);
+  while(i<n1 && j<n2) {
+    if(a[i] <= b[j]) {
+      if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+        unionArr.push_back(a[i]);
+      }
       i++;
-    }
-    else
-    {
-      if (Union.empty() || Union.back() != arr2[j])
-        Union.push_back(arr2[j]);
+    } else {
+      if(unionArr.size() == 0 || unionArr.back() != b[j]) {
+        unionArr.push_back(b[j]);
+      }
       j++;
     }
   }
 
-  while (i < n)
+  while (j < n2)
   {
-    if (Union.empty() || Union.back() != arr1[i])
-      Union.push_back(arr1[i]);
-    i++;
-  }
-
-  while (j < m)
-  {
-    if (Union.empty() || Union.back() != arr2[j])
-      Union.push_back(arr2[j]);
+    if (unionArr.size() == 0 || unionArr.back() != b[j])
+    {
+      unionArr.push_back(b[j]);
+    }
     j++;
   }
 
-  return Union;
+  while (i < n1)
+  {
+    if (unionArr.size() == 0 || unionArr.back() != a[i])
+    {
+      unionArr.push_back(a[i]);
+    }
+    i++;
+  }
+
+  return unionArr;
 }
+/*
+time complexity = O(n1 + n2)
+space complexity = O(n1 + n2) // for returning the array
+*/
 
 int main()
 {
-  int n = 10, m = 7;
-  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+  vector <int> arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  vector <int> arr2 = {2, 3, 4, 4, 5, 11, 12};
 
-  vector<int> Union = FindUnion(arr1, arr2, n, m);
+  vector<int> Union = FindUnion(arr1, arr2);
 
   cout << "Union of arr1 and arr2 is:\n";
   for (auto &val : Union)
@@ -68,11 +74,11 @@ int main()
   cout << endl;
 
   // Testing brute force
-  vector<int> bruteUnion = UNION_Brute(vector<int>(arr1, arr1 + n), vector<int>(arr2, arr2 + m));
-  cout << "Union using brute force is:\n";
-  for (auto &val : bruteUnion)
-    cout << val << " ";
-  cout << endl;
+  //vector<int> bruteUnion = UNION_Brute(vector<int>(arr1, arr1 + n), vector<int>(arr2, arr2 + m));
+  //cout << "Union using brute force is:\n";
+  //for (auto &val : bruteUnion)
+    //cout << val << " ";
+  //cout << endl;
 
   return 0;
 }
