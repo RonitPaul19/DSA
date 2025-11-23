90d1f6f77226de11b01d5fa677627aa6a266ad48
Ronit Paul <ronitpaulindia@gmail.com>
2025-11-23 21:58:48 +0530

    recursive implementation of binary search

---
+++ 02_BINARY_SEARCH/01_BS_IN_SORTED_ARRAY/02_BS_usingRecursion.cpp
@@ -0,0 +1,23 @@
+// recursive way of binary search algorithm
+#include <bits/stdc++.h>
+using namespace std;
+
+int BS_recursion(vector<int> a, int target, int low, int high) {
+  if(low > high) return -1;
+  int mid = (low+high)/2;
+
+  if (a[mid] == target)
+  {
+    return mid;
+  }
+  else if(target > a[mid]) {
+    return BS_recursion(a, target, mid+1, high); // low = mid + 1
+  }
+  return BS_recursion(a, target, low, mid-1); // high = mid -1
+}
+
+int main() {
+  vector<int> a = {1, 2, 4, 6, 9, 10};
+  BS_recursion(a, 9, 0, 5);
+  return 0;
+}
