e12abbeb3c45baed6c447b8b630b657131eb26fc
Ronit Paul <ronitpaulindia@gmail.com>
2025-11-25 19:59:33 +0530

    search insert position

--- 02_BINARY_SEARCH/01_BS_IN_SORTED_ARRAY/05_searchInsertPosition.cpp
+++ 02_BINARY_SEARCH/01_BS_IN_SORTED_ARRAY/05_searchInsertPosition.cpp
@@ -6,8 +6,28 @@ target is found. If not, return the index where it would be if it were inserted 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-  
+void searchInsertPositionUtil(const vector<int> &nums, int target, int &ans, int left, int right) {
+  if (left > right) {
+    return;
+  }
+  int mid = left + (right - left) / 2;
+  if (nums[mid] == target) {
+    ans = mid;
+    return;
+  } else if (nums[mid] < target) {
+    ans = mid + 1;
+    searchInsertPositionUtil(nums, target, ans, mid + 1, right);
+  } else {
+    ans = mid;
+    searchInsertPositionUtil(nums, target, ans, left, mid - 1);
+  }
+}
+
+int main() {
+  vector<int> nums = {1, 3, 5, 6};
+  int target = 5;
+  int ans = -1;
+  searchInsertPositionUtil(nums, target, ans, 0, nums.size() - 1);
+  cout << ans << endl;
   return 0;
 }
