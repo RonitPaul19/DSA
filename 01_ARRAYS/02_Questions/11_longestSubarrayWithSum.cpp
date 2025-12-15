3a9f029cd319a1116a430d79a612a007e7046ba7
Ronit Paul <ronitpaulindia@gmail.com>
2025-12-15 12:11:43 +0530

    simple changes

---
+++ 01_ARRAYS/02_Questions/11_longestSubarrayWithSum.cpp
@@ -0,0 +1,12 @@
+// longest subarray with sum k
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> longestSubarray(vector<int> a, int k) {
+
+}
+
+int main() {
+
+  return 0;
+}
---
+++ 02_BINARY_SEARCH/01_BS_IN_SORTED_ARRAY/09_searchInRotatedSortedArray.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> a = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+      
+  return 0;
+}
