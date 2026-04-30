afb4bf0795dbd33e3f84f4b636afa4cd0096318c
Ronit Paul <ronitpaulindia@gmail.com>
2026-04-30 10:36:51 +0530

    added description of the problem along with some time and space complexity

--- 01_ARRAYS/02_Questions/08_missingNumber.cpp
+++ 01_ARRAYS/02_Questions/08_missingNumber.cpp
@@ -1,41 +1,50 @@
-//find the missing number from an array of size (n - 1) for a given value n
-#include <bits/stdc++.h>
+/*
+Given an array arr[] of size n-1 with distinct integers in the range of [1, n].
+This array represents a permutation of the integers from 1 to n with one element
+missing. Find the missing element in the array.
+*/
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int findMissing_BRUTE(vector<int> a, int n) {
-  int sumOfN = 0;
-  int sumOfArray = 0;
-
+int missingNum_BRUTE(vector<int>& arr, int n) {
   for (int i = 1; i <= n; i++) {
-    sumOfN += i;
-  }
-  for (int i = 0; i < a.size(); i++) {
-    sumOfArray += a[i];
+    bool found = false;
+
+    for (int j = 0; j < arr.size(); j++) {
+      if (arr[j] == i) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) return i;
   }
 
-  return (sumOfN - sumOfArray);
+  return -1;
 }
 
-int findMissing_OPTIMAL(vector<int> a, int n) {
-  int xor1 = 0;
-  int xor2 = 0;
-  for(int i = 1; i <= n; i++) { // xor of max bound of number
+int missingNum_OPTIMAL(vector<int>& arr, int n) {
+  int xor1 = 0, xor2 = 0;
+
+  // XOR all array elements
+  for (int i = 0; i < n - 1; i++) {
+    xor2 ^= arr[i];
+  }
+
+  // XOR all numbers from 1 to n
+  for (int i = 1; i <= n; i++) {
     xor1 ^= i;
   }
-  for(int i = 0; i < a.size(); i++) { // xor of all the elements in the array
-    xor2 ^= a[i];
-  }
-  return xor1^xor2;
+
+  // Missing number is the XOR of xor1 and xor2
+  return xor1 ^ xor2;
 }
 
 int main() {
-  int n = 5;
-  vector<int> a = {1, 2, 4, 5};
-  int x = findMissing_BRUTE(a, n);
-  cout<<x;
-  cout<<endl;
-  int y = findMissing_OPTIMAL(a, n);
-  cout<<y;
-
+  vector<int> arr = {8, 2, 4, 5, 3, 7, 1};
+  int n = 8;
+  int res = missingNum_OPTIMAL(arr, n);
+  cout << res << endl;
   return 0;
 }
