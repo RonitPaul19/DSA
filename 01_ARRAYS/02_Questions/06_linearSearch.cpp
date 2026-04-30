60055974d9eb9b3ebdba678e634bd6304c96c629
Ronit Paul <ronitpaulindia@gmail.com>
2026-04-30 10:34:16 +0530

    Added STL vectors instead of arrays

--- 01_ARRAYS/02_Questions/06_linearSearch.cpp
+++ 01_ARRAYS/02_Questions/06_linearSearch.cpp
@@ -1,24 +1,23 @@
-//linear searching algorithm in arrays
+// linear search
 #include <bits/stdc++.h>
 using namespace std;
 
-int linearSearch(int a[], int n, int x) {
-  for (int i = 0; i < n; i++) {
-  if (a[i] == x) {
-      return i;
-    }
+int linearSearch(vector<int> a, int x) {
+  for (int i = 0; i < a.size(); i++) {
+    if (a[i] == x) return i;
   }
-  return -1;
+  return 0;
 }
 
 int main() {
-  int a[] = {1, 3, 2, 5, 7};
-  int n = sizeof(a)/sizeof(int);
-  int x = linearSearch(a, n, 5);
-  if(x == -1) {
-    cout<<"Element not found";
-  } else {
-  cout<<"Found at index "<<x;
-  }
+  vector<int> a = {1, 8, 3, 5, 2, 9, 7};
+
+  int result = linearSearch(a, 10);
+
+  if (result)
+    cout << "Element found in index " << result;
+  else
+    cout << "Element not found";
+
   return 0;
 }
