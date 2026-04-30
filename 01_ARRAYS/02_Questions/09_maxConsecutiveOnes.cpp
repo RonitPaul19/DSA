587de33a13efa07bad391444b7ceff2430388e92
Ronit Paul <ronitpaulindia@gmail.com>
2026-04-30 10:37:57 +0530

    Added desciption of the algorithm along with time and space somplexity

--- 01_ARRAYS/02_Questions/09_maxConsecutiveOnes.cpp
+++ 01_ARRAYS/02_Questions/09_maxConsecutiveOnes.cpp
@@ -1,26 +1,31 @@
-// maximum consecutive ones
+// Maximum consecutive ones
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxConsecutiveOnes(vector<int> a) {
+// TC = O(n)
+int maximumConsecutiveOnes(vector<int> arr) {
   int maxi = 0;
   int cnt = 0;
-  for (int i = 0; i < a.size(); i++)
-  {
-    if (a[i] == 1)
-    {
+
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] == 1) {
       cnt++;
-      maxi = max(maxi, cnt);
+      maxi =
+          max(cnt, maxi);  // to find and take the largest between cnt and maxi
     } else {
-      cnt = 0;
+      cnt = 0;  // if 1 not found that means consecutiveness is broken and cnt
+                // reset to 0
     }
   }
+
   return maxi;
 }
 
 int main() {
-  vector<int> a = {1, 1, 0, 1, 1, 1, 0, 1, 1};
-  int x = maxConsecutiveOnes(a);
-  cout<<x;
+  vector<int> arr = {1, 1, 1, 0, 1, 1, 1, 0, 1, 1};  // the answer will be 3
+
+  int result = maximumConsecutiveOnes(arr);
+  cout << result;
+
   return 0;
 }
