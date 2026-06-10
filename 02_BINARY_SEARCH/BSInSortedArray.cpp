ef1db76c413d3ea6906df3f9b535a23ba8c5b0d5
RonitPaul19 <ronitpaulindia@gmail.com>
2026-06-10 11:28:02 +0530

    binary search in a sorted array

--- 02_BINARY_SEARCH/BSInSortedArray.cpp
+++ 02_BINARY_SEARCH/BSInSortedArray.cpp
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int bs(int arr[], int n, int el) {
+int bs(const vector<int>& arr, int el) {
+  int n = arr.size();
+
   int low = 0;
   int high = n - 1;
 
   while (low <= high) {
     int mid = low + (high - low) / 2;
 
-    if (arr[mid] == el) {
+    if (arr[mid] == el)
       return mid;
-    } else if (el < arr[mid]) {
+    else if (el < arr[mid])
       high = mid - 1;
-    } else {
+    else
       low = mid + 1;
-    }
   }
 
   return -1;
 }
 
 int main() {
-  const int n = 6;
-  int arr[n] = {1, 2, 4, 6, 8, 9};
+  vector<int> a = {1, 2, 4, 6, 8, 9};
 
   int el = 8;
 
-  int result = bs(arr, n, el);
+  int result = bs(a, el);
 
   cout << result << endl;
 
